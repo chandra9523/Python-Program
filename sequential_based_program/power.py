449e17310a8f0039a5af331c968b3e79d8a5fd51
Chandra Bhushan <chandra.bhushan9523@gmail.com>
2026-03-29 09:23:46 +0530

    added program

---
+++ sequential_based_program/power.py
@@ -0,0 +1,10 @@
+#write a python program to find the power of a number(x^y)
+
+num = int(input("enter number = "))
+exponent = int(input("enter exponent = "))
+
+power = num**exponent
+
+print(f"Power of {num} with raise to exponent {exponent}= {power}")
+
+
